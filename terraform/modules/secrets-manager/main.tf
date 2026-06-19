46daae5b51f97d36cba8c1021230d6f379568a1a
Vinay <kondojuvinaykumar2004@gmail.com>
2026-06-19 07:06:45 +0530

    fix: set recovery_window_in_days to 0 for dev secrets

--- terraform/modules/secrets-manager/main.tf
+++ terraform/modules/secrets-manager/main.tf
@@ -1,8 +1,9 @@
 resource "aws_secretsmanager_secret" "app_secrets" {
-  for_each   = toset(var.services)
-  name       = "procurement/${var.environment}/${each.value}"
-  kms_key_id = var.kms_key_arn
-  tags       = var.tags
+  for_each                = toset(var.services)
+  name                    = "procurement/${var.environment}/${each.value}"
+  kms_key_id              = var.kms_key_arn
+  recovery_window_in_days = 0
+  tags                    = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "app_secrets_val" {
